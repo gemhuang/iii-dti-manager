842699dbb5de2d85a43e9cfc9b58eebb3824bf6c
gemhuang <gemhuang@iii.org.tw>
2020-09-21 15:34:47 +0800

    #1 加入基本頁面

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import DashboardLayout from 'src/commons/layout/DashboardLayout';
 import MainLayout from 'src/commons/layout/MainLayout';
+import { NotFoundView } from 'src/commons/view/NotFoundView';
+import LoginView from 'src/auth/LoginView';
+import RegisterView from 'src/auth/RegisterView';
+import AccountView from 'src/system/account/AccountView';
+import SettingsView from 'src/system/setting/SettingsView';
 
 const routes = [
   {
     path: 'app',
     element: <DashboardLayout />,
     children: [
+      { path: 'account', element: <AccountView /> },
       { path: 'settings', element: <SettingsView /> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
@@ -22,6 +28,16 @@ const routes = [
       { path: '/', element: <Navigate to="/app/dashboard" /> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
+  },
+  {
+    path: 'login',
+    element: <LoginView />,
+    children: []
+  },
+  {
+    path: 'register',
+    element: <RegisterView />,
+    children: []
   }
 ];
 
