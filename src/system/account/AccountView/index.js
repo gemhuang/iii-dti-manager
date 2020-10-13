4b84bf007931b3c693c3e0ea4121dff6ff55dfd9
ChunChun Huang <cchuang.1024@zoho.com>
2020-10-14 01:22:24 +0800

    update structure

--- src/system/account/AccountView/index.js
+++ src/system/account/AccountView/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Grid } from '@material-ui/core';
+import { Container, Grid, makeStyles } from '@material-ui/core';
 import Page from 'src/commons/component/Page';
 import Profile from 'src/system/account/AccountView/Profile';
 import ProfileDetails from 'src/system/account/AccountView/ProfileDetails';
