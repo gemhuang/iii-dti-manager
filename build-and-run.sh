f910ef1526b4fde0808a2fb19aa0a3311ef9e228
ChunChun Huang <cchuang.1024@zoho.com>
2021-01-18 00:30:29 +0800

    change to mui

--- build-and-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -d dti-manager .
-docker run -d -t --name dti-manager -p 3000:3000 dti-manager
-
--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import 'react-perfect-scrollbar/dist/css/styles.css';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
 
-import { useRoutes } from 'react-router-dom';
-import routes from './routes';
-
-import './App.scss';
-
-const App = () => {
-  const routing = useRoutes(routes);
-
+function App() {
   return (
-    <div>{routing}</div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
