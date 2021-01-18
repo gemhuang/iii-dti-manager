4ca0fbc9e29cd93c991b49ab61e1158d4f1c9c7a
ChunChun Huang <cchuang.1024@zoho.com>
2021-01-19 00:21:31 +0800

    update dependency

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createBrowserHistory } from 'history'
+import { Root, configureStore } from './root'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const history = createBrowserHistory()
+const store = configureStore(history)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<Root store={store} history={history} />, document.getElementById('root'))
---
+++ src/root/Root.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import Routes from './Routes'
+
+const Root = ({ store, history }) => (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Routes />
+      </ConnectedRouter>
+    </Provider>
+)
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
+}
+export default Root
